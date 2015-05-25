50559ad3721332f1b86843fd5c4042b27b808630
evgenitochev <evgenitochev@gmail.com>
2015-05-25 14:33:52 +0300

    Finished Router

--- Core/Classes/Router.php
+++ Core/Classes/Router.php
@@ -17,7 +17,31 @@ class Router
         if (count($params) > 1) {
             $exploded_route = explode("/", $route);
             if (count($params) == count($exploded_route)) {
-
+                $matching = true;
+                //PARAMETERS TO ADD TO THE METHOD
+                $params_to_method = [];
+                for ($i = 0; $i < count($exploded_route); $i++) {
+                    if ($exploded_route[$i] != $params[$i]) {
+                        $is_paramater = false;
+                        //CHECKING IF THERE IS AN DYNAMIC PARAMETER IN THE URL
+                        if (substr($exploded_route[$i], 0, 1) == "{" && substr($exploded_route[$i],
+                                strlen($exploded_route[$i]) - 1, 1) == "}"
+                        ) {
+                            $param_index = substr($exploded_route[$i], 1, strlen($exploded_route[$i]) - 2);
+                            $is_paramater = true;
+                        }
+                        if (!$is_paramater) {
+                            $matching = false;
+                        } else {
+                            //ADDING PARAMETER VALUE TO THE PARAMETERS
+                            $params_to_method[$param_index] = $params[$i];
+                        }
+                    }
+                }
+                if ($matching) {
+                    //CALLING THE METHOD WITH PARAMETERS
+                    echo $this->callMethod($controller, $method, $params_to_method);
+                }
             }
         } else {
             //IF ROUTE PARAMETER IS EQUAL TO THE ROUTE
@@ -28,14 +52,15 @@ class Router
         }
     }
 
-    private function callMethod($controller, $method)
+    private function callMethod($controller, $method, $params_to_method = null)
     {
         //FULL NAMESPACE OF THE CLASS
         $controller = $this->controllers_namespace . "\\" . $controller;
         //MAKING A NEW INSTANCE
         $instance = new $controller();
+
         //CALLING AND RETURINING THE MEHTOD
-        return $instance->$method();
+        return $instance->$method($params_to_method);
     }
 
 }
