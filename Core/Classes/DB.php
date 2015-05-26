376cd102ae28b7bf15dabcc219cdfb91d463234f
evgenitochev <evgenitochev@gmail.com>
2015-05-26 20:32:35 +0300

    DB vars put in another file

--- Core/Classes/DB.php
+++ Core/Classes/DB.php
@@ -9,7 +9,7 @@ class DB
     public static function query($query)
     {
         if (self::$con == null) {
-            self::$con = mysqli_connect('localhost', 'root', 'root', 'api');
+            self::$con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
         }
 
         return self::$con->query($query);
