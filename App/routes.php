bba6cbc8847e68bdb7ca28345105cac988062955
evgenitochev <evgenitochev@gmail.com>
2015-05-26 19:38:27 +0300

    routes.php coded

---
+++ App/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+$router = new \App\Classes\Router();
+$router->get('jobs/list', 'JobController', 'showAll');
+$router->get('jobs/{id}', 'JobController', 'show');
+$router->get('candidates/list', 'CandidateController', 'showAll');
+$router->get('candidates/review/{id}', 'CandidateController', 'review');
+$router->get('candidates/review/{id}', 'CandidateController', 'search');
