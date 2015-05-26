69e8b13ca030f28f90ac31eaefb405d06d79214e
evgenitochev <evgenitochev@gmail.com>
2015-05-26 16:18:41 +0300

    Job Controller Logic

--- App/Controllers/JobController.php
+++ App/Controllers/JobController.php
@@ -1,11 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: yoan
- * Date: 15-5-25
- * Time: 14:06
- */
+namespace App\Controllers;
 
-class JobController {
+use App\Classes\DB;
 
+class JobController
+{
+    public function showAll()
+    {
+        $result = DB::query("SELECT * FROM jobs");
+        $return_arr = [];
+        if ($result->num_rows) {
+            while ($row = $result->fetch_assoc()) {
+                array_push($return_arr, $row);
+            }
+        }
+
+        return json_encode($return_arr);
+    }
+
+    public function show($params)
+    {
+        $result = DB::query("SELECT * FROM jobs WHERE id = " . $params['id']);
+        return json_encode($result->fetch_assoc());
+    }
 }
